89d3f3f07cfd17de4e5de73810d270a6b71fc3f0
baAhum <52484799+baAhum@users.noreply.github.com>
2020-07-25 20:40:11 +0200

    SongPlayerAdded

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ var imgAr = [70, 24, 31, 88, 25, 80, 44, 62, 49, 27, 35, 55, 29, 18, 7, 12, 84, 
 var last_changed = [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000];
 var last_changed_pos = 0;
 
+var audioEl = document.getElementById("songs"); 
+var audioName = document.getElementById('audioDesc');
+var audioIco = document.getElementById('audioIco');
+
+var newSong = "none";
+
+var songs_dict = new Object();
+
+songs_dict["Gabby"] = "Wild Heart - Daughtry";
+songs_dict["Charlize"] = "none";
+songs_dict["Symeion"] = "Ride It Out - Jaden Hamilton";
+songs_dict["Matt"] = "Start Of Something Good - Daughtry";
+songs_dict["Janre"] = "Sacred - Citizen Soldier";
+songs_dict["Ma"] = "none";
+songs_dict["Pa"] = "Spirit Lead Me Lyrics - Michael Ketterer";
+
+
 function getRandomImage(imgAr, num_random_img, elements, element_num) {
     path = "url('./MarneOnly/"; // default path here
     var num = Math.floor( Math.random() * num_random_img )+1;
@@ -23,13 +40,13 @@ function assign_random() {
     var amount_to_change = 4;
 
     if (num<10) {
-        amount_to_change = 5;
+        amount_to_change = 4;
     } else if (num<16) {
-        amount_to_change = 6;
+        amount_to_change = 5;
     } else if (num<18) {
-        amount_to_change = 7;
+        amount_to_change = 6;
     } else {
-        amount_to_change = 8;
+        amount_to_change = 7;
     }
 
     for (var i = 0; i < amount_to_change; i++) {
@@ -40,7 +57,6 @@ function assign_random() {
         img_div[i] = temp_Var;
         last_changed[last_changed_pos] = temp_Var;
         last_changed_pos += 1;
-        console.log(last_changed);
         if (last_changed_pos==14) {
             last_changed_pos = 0;
         }
@@ -50,6 +66,43 @@ function assign_random() {
 
 setInterval(function() {
     assign_random();
-}, 4000);
+}, 2200);
+
+function audioControls(){
+    if (newSong=="none") {
+        audioEl.pause();
+        audioName.innerHTML = "No Song Selected";
+        audioIco.setAttribute('src', './Icons/play.svg');
+    } else {
+        if(audioIco.getAttribute('src')=='./Icons/play.svg') {
+            audioName.innerHTML = newSong;
+            audioEl.play();
+            audioIco.setAttribute('src', './Icons/pause.svg');
+        } else {
+            audioEl.pause();
+            audioIco.setAttribute('src', './Icons/play.svg');
+        }
+    }
+}
+
+function PlayFriendAudio(clicked_id){
+    audioEl.pause();
+    newSong = songs_dict[clicked_id];
+    if (newSong!="none") {
+        audioEl.setAttribute('src', './songs/'+newSong+'.m4a');
+        audioName.innerHTML = newSong;
+        audioEl.play();
+        audioIco.setAttribute('src', './Icons/pause.svg');
+    } else {
+        audioName.innerHTML = "No Song Selected";
+        audioIco.setAttribute('src', './Icons/play.svg');
+    }
+}
+
+// function Setup(){
+//     var body = document.getElementById("bodyMain");
+
+//     body.requestFullscreen();
+// }
 
 
