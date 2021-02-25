9f0ef29d0b1f94c3bb22a037e30437eb28b4ae81
mattHydraDet <matthieu@hydradet.com>
2021-02-25 14:36:31 +0200

    Added Gallery
    Gallery has been added with a top category selection and smoother transition of the sticky navbar.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.onscroll = function() {myFunction()};
 window.onload = function() {startFunction()};
 
 var navbar = document.getElementById("nav");
+var curPage = "home";
 var navimg = document.getElementById("navimg");
 var imgheader = document.getElementById("imgheader");
 var sticky = navbar.offsetTop;
@@ -24,7 +25,7 @@ function load(e, page, id) {
 }
 
 function navLoad(e, page) {
-  console.log('\t\t\tcase "' + e.id + '":\n\t\t\t\tbreak;')
+  console.log('\t\t\tcase "' + e.id + '":\n\t\t\t\tbreak;');
   load(innerBody, page, e.id);
 }
 
@@ -38,12 +39,33 @@ function myFunction() {
   var scrollPosition = window.pageYOffset;
 
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
     imgheader.style.opacity = 0;
+    
+
+    sidenavs = document.getElementById('catnav');
+    if (sidenavs) {
+      sidenavs.classList.add("sticky");
+      innerBody.classList.add("sticky_offsetS");
+    } else {
+      innerBody.classList.add("sticky_offset");
+    }
+    
+    
     // navimg.style.opacity = 100;
   } else {
     navbar.classList.remove("sticky");
     imgheader.style.opacity = 100;
+    
+
+    sidenavs = document.getElementById('catnav');
+    if (sidenavs) {
+      sidenavs.classList.remove("sticky");
+      innerBody.classList.remove("sticky_offsetS");
+    } else {
+      innerBody.classList.remove("sticky_offset");
+    }
+    
     // navimg.style.opacity = 0;
   }
 }
@@ -76,45 +98,66 @@ function getPage(e) {
   switch(this.id) {
 
     case "galleryMN":
+      navLoad(this, '/html/gallery.html');
       break;
       case "gl-mu":
+        subLoad('/html/gallery.html', "galleryMN", "gl-mu");
 				break;
  			case "gl-nails":
+        subLoad('/html/gallery.html', "galleryMN", "gl-nails");
 				break;
  			case "gl-hair":
+        subLoad('/html/gallery.html', "galleryMN", "gl-hair");
 				break;
  			case "gl-sfx":
+        subLoad('/html/gallery.html', "galleryMN", "gl-sfx");
 				break;
  			case "gl-ws":
+        subLoad('/html/gallery.html', "galleryMN", "gl-ws");
 				break;
 
     case "servicesMN":
+      navLoad(this, '/html/services.html');
       break;
       case "bas-mu":
+        subLoad('/html/services.html', "servicesMN", "bas-mu");
 				break;
  			case "bas-nails":
+        subLoad('/html/services.html', "servicesMN", "bas-nails");
 				break;
  			case "bas-hair":
+        subLoad('/html/services.html', "servicesMN", "bas-hair");
 				break;
  			case "bas-sfx":
+        subLoad('/html/services.html', "servicesMN", "bas-sfx");
 				break;
  			case "bas-ws":
+        subLoad('/html/services.html', "servicesMN", "bas-ws");
 				break;
 
     case "shopMN":
+      navLoad(this, '/html/shop.html');
       break;
       case "sh-mu":
+        subLoad('/html/shop.html', "shopMN", "sh-mu");
 				break;
  			case "sh-sfx":
+        subLoad('/html/shop.html', "shopMN", "sh-sfx");
 				break;
  			case "sh-kits":
+        subLoad('/html/shop.html', "shopMN", "sh-kits");
 				break;
 
     case "abtusMN":
+      navLoad(this, '/html/about_us.html');
       break;
     default:
       navLoad(this, '/html/home.html');      
       break;
   }
 
+}
+
+function filtFunc(e) {
+  console.log("Check box id =" + e.id);
 }
